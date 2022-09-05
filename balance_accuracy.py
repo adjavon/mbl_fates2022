fe97d3f9ad8c2b9219124abc4a093bba9c566d29
yuningw268 <wangyn530@gmail.com>
2022-09-05 14:47:41 +0000

    Add a code to calculate balanced accuracy

---
+++ balance_accuracy.py
@@ -0,0 +1,19 @@
+
+
+def balance_accuracy(gt_ls, pred_ls):
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+   for i in gt_ls:
+       if gt_ls[i] == 1 and pred_ls[i] == 1:
+            tp += 1
+        elif gt_ls[i] == 1 and pre_ls[i] == 0:
+            fn += 1
+        elif gt_ls[i] == 0 and pred_ls[i] == 1:
+            fp += 1
+        else:
+            tn += 1
+    balanced_accuracy = (tp/(tp + fn) + tn/(tn + fp))/2
+    return balanced_accuracy
+
